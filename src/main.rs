3dcf4a8209bc2c39e50f6c21ef65a099cc2f38a5
Maurice Yang <mauriceyangmy28@gmail.com>
2024-04-17 14:00:16 +0700

    feat: Migrating to shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
-use dotenvy::dotenv;
-// use rocket_cors::AllowedOrigins;
-use sqlx::postgres::PgPoolOptions;
-use std::env;
-use std::error::Error;
-
 #[macro_use]
 extern crate rocket;
+
+use dotenvy::dotenv;
+use shuttle_rocket::ShuttleRocket;
+use sqlx::postgres::PgPoolOptions;
+// use rocket_cors::AllowedOrigins;
+// use std::env;
 
 pub mod guard;
 pub mod model;
 pub mod route;
 pub mod typing;
 
-#[rocket::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+#[shuttle_runtime::main]
+async fn main(#[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore) -> ShuttleRocket {
     dotenv().ok();
-    let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    // let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+
+    let url = secrets.get("DATABASE_URL").unwrap();
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&url)
-        .await?;
+        .await
+        .unwrap();
 
     // let allowed_origins = AllowedOrigins::some_exact(&["https://www.heymart-c14.com"]);
 
@@ -30,16 +33,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // }
     // .to_cors()?;
 
-    let _rocket = rocket::build()
+    let rocket = rocket::build()
         .manage(pool)
         // .attach(cors)
         .mount(
             "/auth",
             routes![route::login, route::register, route::authorize],
         )
-        .mount("/", routes![route::get_user])
-        .launch()
-        .await?;
+        .mount("/", routes![route::get_user]);
 
-    Ok(())
+    Ok(rocket.into())
 }
