25f2e72d87ffbce707ed3184922f75cc588ba3e4
Maurice Yang <mauriceyangmy28@gmail.com>
2024-04-30 15:38:35 +0700

    Refactor: Refactoring verify_token to a reusable function

--- src/guard.rs
+++ src/guard.rs
@@ -23,15 +23,32 @@ pub struct JWT {
     pub claims: Claims,
 }
 
+fn is_valid(key: &str) -> Result<Claims, Error> {
+    Ok(decode_jwt(String::from(key))?)
+}
+
+pub fn verify_token(token: &str) -> Result<Claims, String> {
+    match is_valid(token) {
+        Ok(claims) => Ok(claims),
+        Err(err) => match &err.kind() {
+            jsonwebtoken::errors::ErrorKind::ExpiredSignature => {
+                return Err("Error validating JWT token - Expired Token".to_string());
+            }
+            jsonwebtoken::errors::ErrorKind::InvalidToken => {
+                return Err("Error validating JWT token - Invalid Token".to_string());
+            }
+            _ => {
+                return Err(format!("Error validating JWT token - {}", err));
+            }
+        },
+    }
+}
+
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for JWT {
     type Error = NetworkResponse;
 
     async fn from_request(req: &'r Request<'_>) -> Outcome<Self, NetworkResponse> {
-        fn is_valid(key: &str) -> Result<Claims, Error> {
-            Ok(decode_jwt(String::from(key))?)
-        }
-
         match req.headers().get_one("authorization") {
             None => {
                 let response = Response {
@@ -44,50 +61,17 @@ impl<'r> FromRequest<'r> for JWT {
                     NetworkResponse::Unauthorized(serde_json::to_string(&response).unwrap()),
                 ))
             }
-            Some(key) => match is_valid(key) {
+            Some(key) => match verify_token(key) {
                 Ok(claims) => Outcome::Success(JWT { claims }),
-                Err(err) => match &err.kind() {
-                    jsonwebtoken::errors::ErrorKind::ExpiredSignature => {
-                        let response = Response {
-                            body: ResponseBody::Message(format!(
-                                "Error validating JWT token - Expired Token"
-                            )),
-                        };
-                        Outcome::Error((
-                            Status::Unauthorized,
-                            NetworkResponse::Unauthorized(
-                                serde_json::to_string(&response).unwrap(),
-                            ),
-                        ))
-                    }
-                    jsonwebtoken::errors::ErrorKind::InvalidToken => {
-                        let response = Response {
-                            body: ResponseBody::Message(format!(
-                                "Error validating JWT token - Invalid Token"
-                            )),
-                        };
-                        Outcome::Error((
-                            Status::Unauthorized,
-                            NetworkResponse::Unauthorized(
-                                serde_json::to_string(&response).unwrap(),
-                            ),
-                        ))
-                    }
-                    _ => {
-                        let response = Response {
-                            body: ResponseBody::Message(format!(
-                                "Error validating JWT token - {}",
-                                err
-                            )),
-                        };
-                        Outcome::Error((
-                            Status::Unauthorized,
-                            NetworkResponse::Unauthorized(
-                                serde_json::to_string(&response).unwrap(),
-                            ),
-                        ))
-                    }
-                },
+                Err(err) => {
+                    let response = Response {
+                        body: ResponseBody::Message(err),
+                    };
+                    Outcome::Error((
+                        Status::Unauthorized,
+                        NetworkResponse::Unauthorized(serde_json::to_string(&response).unwrap()),
+                    ))
+                }
             },
         }
     }
