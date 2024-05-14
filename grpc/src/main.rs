f81a3b06c7922914eb07fdd6f2d2a356dd02ea6b
Maurice Yang <mauriceyangmy28@gmail.com>
2024-05-14 20:44:14 +0700

    feat: Adding new function for updating balance

--- grpc/src/main.rs
+++ grpc/src/main.rs
@@ -1,23 +1,40 @@
-use crate::{auth::MyAuthService, services::auth_service_server::AuthServiceServer};
+use std::env;
+
+use crate::{
+    auth::MyAuthService, services::auth_service_server::AuthServiceServer,
+    services::user_service_server::UserServiceServer, user::MyUserService,
+};
 use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
 use tonic::transport::Server;
 
 pub mod services {
     tonic::include_proto!("services");
 }
 pub mod auth;
+pub mod user;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
+    let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(3)
+        .connect(&url)
+        .await
+        .unwrap();
+
     let addr = "0.0.0.0:8080".parse().unwrap();
     let auth_service = MyAuthService::default();
+    let user_service = MyUserService { pool };
 
     println!("Server listening on {}", addr);
 
     Server::builder()
         .add_service(AuthServiceServer::new(auth_service))
+        .add_service(UserServiceServer::new(user_service))
         .serve(addr)
         .await?;
 
---
+++ grpc/src/user.rs
@@ -0,0 +1,78 @@
+use shared::verify_token;
+use tonic::{Request, Response, Status};
+
+use crate::services::{
+    user_service_server::UserService, UpdateBalanceRequest, UpdateBalanceResponse,
+};
+
+pub struct MyUserService {
+    pub pool: sqlx::PgPool,
+}
+
+#[tonic::async_trait]
+impl UserService for MyUserService {
+    async fn update_balance(
+        &self,
+        req: Request<UpdateBalanceRequest>,
+    ) -> Result<Response<UpdateBalanceResponse>, Status> {
+        let body = req.into_inner();
+        let claims = match verify_token(&body.token) {
+            Ok(result) => result,
+            Err(_) => return Err(Status::unauthenticated("Invalid Token".to_string())),
+        };
+
+        if body.amount < 0 {
+            return Err(Status::invalid_argument("Invalid amount.".to_string()));
+        }
+
+        match body.transaction_type {
+            0 => {
+                let user = sqlx::query!("SELECT balance FROM users WHERE id = $1", claims.id)
+                    .fetch_one(&self.pool.clone())
+                    .await
+                    .unwrap();
+                if user.balance.unwrap_or(0) < body.amount {
+                    return Err(Status::invalid_argument(
+                        "Insufficient balance.".to_string(),
+                    ));
+                }
+                let result = match sqlx::query!(
+                    "UPDATE users SET balance = balance - $1 WHERE id = $2 RETURNING balance",
+                    body.amount,
+                    &claims.id
+                )
+                .fetch_one(&self.pool.clone())
+                .await
+                {
+                    Ok(result) => result,
+                    Err(_) => return Err(Status::internal("Database error.".to_string())),
+                };
+                return Ok(Response::new(UpdateBalanceResponse {
+                    success: true,
+                    new_balance: result.balance.unwrap_or_else(|| 0),
+                }));
+            }
+            1 => {
+                // Add balance
+                let result = match sqlx::query!(
+                    "UPDATE users SET balance = balance + $1 WHERE id = $2 RETURNING balance",
+                    body.amount,
+                    &claims.id
+                )
+                .fetch_one(&self.pool.clone())
+                .await
+                {
+                    Ok(result) => result,
+                    Err(_) => return Err(Status::internal("Database error.".to_string())),
+                };
+                return Ok(Response::new(UpdateBalanceResponse {
+                    success: true,
+                    new_balance: result.balance.unwrap_or_else(|| 0),
+                }));
+            }
+            _ => {
+                return Err(Status::invalid_argument("Invalid type.".to_string()));
+            }
+        }
+    }
+}
